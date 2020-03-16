189ed1ec7f3d6483e3c43e3d70b46e137585ccb6
whiteRa2bit <pavel.fakanov@gmail.com>
2020-03-16 22:39:45 +0300

    transfer to typescript

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AudioUploader from './components/AudioUploader'
+
+function App() {
+  return (
+    <div className="App">
+        <AudioUploader />
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
---
+++ src/components/AudioUploader.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Dropzone from 'react-dropzone-uploader'
+import 'react-dropzone-uploader/dist/styles.css'
+
+const AudioUploader = () => {
+  const getUploadParams = ({ meta }) => {
+    const url = 'https://httpbin.org/post'
+    return { url, meta: { fileUrl: `${url}/${encodeURIComponent(meta.name)}` } }
+  }
+
+
+  const upload = (file, title) => {
+    var formData = new FormData();
+    formData.append("title", title);
+    formData.append("file", file)
+
+    fetch('http://localhost:8000/upload', {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+      },
+      body: formData
+    }).then(
+      response => response.json()
+    ).then(
+      success => console.log(success)
+    ).catch(
+      error => console.log(error)
+    );
+  };
+
+  const handleChangeStatus = ({ meta }, status) => {
+    console.log(status, meta)
+  }
+
+  const handleSubmit = (files, allFiles) => {
+    files.forEach(f => upload(f.file, f.meta.name))
+    console.log(files.map(f => f.meta))
+    allFiles.forEach(f => f.remove())
+  }
+
+  return (
+    <Dropzone
+      getUploadParams={getUploadParams}
+      onChangeStatus={handleChangeStatus}
+      onSubmit={handleSubmit}
+      accept="audio/mp3"
+      inputContent={(files, extra) => (extra.reject ? 'WAV files only' : 'Drag your audio file')}
+      styles={{
+        dropzoneReject: { borderColor: 'red', backgroundColor: '#DAA' },
+        inputLabel: (files, extra) => (extra.reject ? { color: 'red' } : {}),
+      }}
+    />
+  )
+}
+
+export default AudioUploader
